97cb271b48df2cf68dc6fbbaa7e581a07668d074
Fawzi Gharib <fawz11gharib@gmail.com>
2024-02-26 19:29:32 +0300

    Assignment Done

---
+++ script.js
@@ -0,0 +1,21 @@
+
+  let adviceId = document.getElementById('advice-id');
+  let adviceContent = document.getElementById('advice-content');
+
+
+
+async function fetchAdvices(){
+  const res = await fetch('https://api.adviceslip.com/advice');
+
+  const { slip:{id,advice } } = await res.json();
+
+  if(res.status==200){
+    adviceId.textContent = `ADVICE #${id}`;
+  adviceContent.textContent = `“${advice}”`;
+  }
+
+}
+
+
+fetchAdvices();
+
